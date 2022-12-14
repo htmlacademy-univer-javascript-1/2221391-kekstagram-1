2bc689f67ed809a6cbbe949e745fb9884fe48952
Сергей <sery66666@mail.ru>
2022-12-14 21:38:34 +0500

    Перламутровые пуговицы

--- js/data.js
+++ js/data.js
@@ -1,43 +1,3 @@
-import { getRandomElement, getRandomNumber } from './util.js';
-
-const NAMES = ['Анастасия', 'Александр', 'Семён', 'Евгения', 'Наталья', 'Валерий', 'Дарья' ];
-
-const MESSAGES = ['Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-const DESCRIPTION = ['Балда какая-то пикнула', 'На прогулке', 'Иииииииуууууууу', 'Тут должно быть описание...',
-  'Живу жизнь, а вы чем занимаетесь?', 'Отвлекаюсь', 'Посиделки с друзьями', 'ЖОЗЗЗ'];
-
-let idComm = 0;
-const createComment = () =>{
-  idComm++;
-  return{
-    id: idComm,
-    avatar: `img/avatar-${getRandomNumber(1,6)}.svg`,
-    message: getRandomElement(MESSAGES),
-    name: getRandomElement(NAMES)
-  };
-
-};
-
-let idNumber = 0;
-const createPhotoDescription = () => {
-  idNumber++;
-  return {
-    id: idNumber,
-    url: `photos/${idNumber}.jpg`,
-    description: getRandomElement(DESCRIPTION),
-    likes: getRandomNumber(15,200),
-    comments:  Array.from({length: getRandomNumber(1,25) }, createComment)
-  };
-};
-
-const arrayPhotos = () => Array.from({length: 25 }, createPhotoDescription);
-
 const MAX_STRING_LENGTH = 140;
 const MAX_HASHTAG_COUNT = 5;
 const MAX_HASHTAG_LENGTH = 20;
@@ -120,4 +80,12 @@ const Effects = {
     }
   }
 };
-export{arrayPhotos, ErrorMessage, MAX_HASHTAG_COUNT, MAX_HASHTAG_LENGTH, MAX_STRING_LENGTH, DEFAULT_COMMENTS_COUNT,UPLOAD_COMMENTS_COUNT, ScaleImg,Effects};
+const TIMEOUT_DELAY = 500;
+const MAX_COUNT_RANDOM_PHOTO = 10;
+const Filter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
+};
+
+export{ ErrorMessage, MAX_HASHTAG_COUNT, MAX_HASHTAG_LENGTH, MAX_STRING_LENGTH, DEFAULT_COMMENTS_COUNT,UPLOAD_COMMENTS_COUNT, ScaleImg,Effects, TIMEOUT_DELAY, MAX_COUNT_RANDOM_PHOTO,Filter};
